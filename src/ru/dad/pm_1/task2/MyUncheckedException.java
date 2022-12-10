01b74b32dd2cc0db86268a5fdedddd3159657023
Dad8875 <Serstep1956@yahoo.com>
2022-12-10 10:37:44 +0300

    solution 4.task2

---
+++ src/ru/dad/pm_1/task2/MyUncheckedException.java
@@ -0,0 +1,8 @@
+package ru.dad.pm_1.task2;
+
+/*
+2. Создать собственное исключение MyUncheckedException, являющееся непроверяемым.
+*/
+public class MyUncheckedException extends RuntimeException {
+
+}
