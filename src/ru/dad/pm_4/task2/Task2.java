bad78710a78409595fac839a2e9a7403531608e8
Dad8875 <Serstep1956@yahoo.com>
2022-12-23 00:51:34 +0300

    Solution 4.3.task2

---
+++ src/ru/dad/pm_4/task2/Task2.java
@@ -0,0 +1,18 @@
+package ru.dad.pm_4.task2;
+
+import java.util.List;
+
+/*
+2. На вход подается список целых чисел. Необходимо вывести результат перемножения
+этих чисел.
+Например, если на вход передали List.of(1, 2, 3, 4, 5), то результатом должно быть число
+120 (т.к. 1 * 2 * 3 * 4 * 5 = 120).
+
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        int result = list.stream().mapToInt(Integer::intValue).reduce(1, (a, b) -> a * b);
+        System.out.println(result);
+    }
+}
