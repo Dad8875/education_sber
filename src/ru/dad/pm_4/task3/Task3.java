17dde4a37e3aaccc6b4c75d5d5551f292fbcad3d
Dad8875 <Serstep1956@yahoo.com>
2022-12-25 20:54:47 +0300

    refactoring SOlution 4.3.task3

--- src/ru/dad/pm_4/task3/Task3.java
+++ src/ru/dad/pm_4/task3/Task3.java
@@ -1,7 +1,6 @@
 package ru.dad.pm_4.task3;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /*
 3. На вход подается список строк. Необходимо вывести количество непустых строк в
@@ -11,7 +10,6 @@ import java.util.stream.Collectors;
 public class Task3 {
     public static void main(String[] args) {
         List<String> list = List.of("abc", "", "def", "ggg", "java", "", "empty", "");
-        List<String> collect = list.stream().filter(s -> (!s.isEmpty())).collect(Collectors.toList());
-        System.out.println(collect.size());
+        System.out.println(list.stream().filter(s -> (!s.isEmpty())).count());
     }
 }
